59e66c6cfdb95f705311b75e254953fe8218bef2
Montherocks <meghna.mukherjee2005@gmail.com>
2026-05-25 13:54:12 +0530

    Delete new_frontend/functions directory

--- new_frontend/functions/[[path]].js
+++
@@ -1,7 +0,0 @@
-import { render } from "../dist/server/worker-entry-BAeIdAfL.js"; 
-// Note: If that hashed string filename changes on your next build, 
-// we will point it to index.js or let TanStack generate a clean entry point.
-
-export async function onRequest(context) {
-  return render(context.request, context.env);
-}
